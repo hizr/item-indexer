e754f87335502c3775fb3bb52aef7798fefcb682
Martin Heinzmann <heizerlein@gmail.com>
2017-12-05 20:30:58 +0100

    change to dozer 6.1.0

--- src/main/java/de/hizr/poe/itemindexer/ItemIndexerApplication.java
+++ src/main/java/de/hizr/poe/itemindexer/ItemIndexerApplication.java
@@ -3,14 +3,16 @@ package de.hizr.poe.itemindexer;
 import java.util.List;
 
 import org.dozer.CustomConverter;
-import org.dozer.spring.DozerBeanMapperFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.client.RestTemplate;
+
+import com.github.dozermapper.spring.DozerBeanMapperFactoryBean;
 
 @SpringBootApplication
 public class ItemIndexerApplication {
@@ -26,6 +28,7 @@ public class ItemIndexerApplication {
 	}
 
 	@Bean
+	@Scope(value = "singleton")
 	public DozerBeanMapperFactoryBean dozerBeanMapperFactoryBean() {
 		final DozerBeanMapperFactoryBean dozerBeanMapperFactoryBean = new DozerBeanMapperFactoryBean();
 
